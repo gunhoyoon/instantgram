1409d0260d02a21428a35aeda9811e36f43b0028
gunhoyoon <rkdus5964@gmail.com>
2023-10-12 22:06:02 +0900

    save

--- src/components/PostUserAvatar.tsx
+++ src/components/PostUserAvatar.tsx
@@ -11,9 +11,7 @@ export default function PostUserAvatar({ image, username }: Props) {
     <div className="flex items-center p-2">
       {/* 글 쓴 사람의 이미지 = userImage , 해당 글에 올린 이미지 = image */}
       <Avatar image={image} size="medium" highlight />
-      <span className="text-gray-900 font-bold ml-2">
-        {username}대충 커밋추가했음
-      </span>
+      <span className="text-gray-900 font-bold ml-2">{username}</span>
     </div>
   );
 }
